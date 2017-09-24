65059f2a9843a15d0a170d6e52bf4401d9ab381f
whtnrms2017 <whtnrms2012@naver.com>
2017-09-24 18:33:33 +0900

    모든 액티비티 세로 고정, 회원가입 정보체크 방식 변경(즉시체크)

--- app/src/main/java/com/kkard/seoulroad/RegistActivity.java
+++ app/src/main/java/com/kkard/seoulroad/RegistActivity.java
@@ -6,10 +6,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +17,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static com.kkard.seoulroad.LoginActivity.checkEmail;
+import java.util.regex.Pattern;
 
 /**
  * Created by SuGeun on 2017-08-08.
@@ -28,8 +28,7 @@ public class RegistActivity extends AppCompatActivity{
     private ConstraintLayout backLayout;
     private Button joinBtn;
     private ImageView backBtn;
-    private TextView toolbarTitle;
-    private boolean passwordFlag = false,passConfFlag= false ,emailFlag= false ,nameFlag= false ;
+    private TextView toolbarTitle, checkName, checkEmail, checkPass, checkPassC;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,11 +39,19 @@ public class RegistActivity extends AppCompatActivity{
         passConf = (EditText)findViewById(R.id.joinPassConf); // 비밀번호 확인
         email = (EditText)findViewById(R.id.joinEmail); // 이메일
         name = (EditText)findViewById(R.id.joinName); // 이름
-        backLayout = (ConstraintLayout)findViewById(R.id.joinBack);
-        backBtn = (ImageView)findViewById(R.id.btn_toolbar_back);
-        toolbarTitle = (TextView)findViewById(R.id.text_toolbar);
+        backLayout = (ConstraintLayout)findViewById(R.id.joinBack);// 뒷배경
+        backBtn = (ImageView)findViewById(R.id.btn_toolbar_back); // 툴바 내 뒤로가기 이미지 버튼
+        toolbarTitle = (TextView)findViewById(R.id.text_toolbar); // 툴바 제목
+        checkName = (TextView)findViewById(R.id.checkName); // 이름 경고창
+        checkEmail = (TextView)findViewById(R.id.checkEmail); // 이메일 경고창
+        checkPass = (TextView)findViewById(R.id.checkPass);  // 비밀번호 경고창
+        checkPassC = (TextView)findViewById(R.id.checkPassC); // 비밀번호 확인 경고창
 
         toolbarTitle.setText("회원가입");
+        checkEmail.setVisibility(View.INVISIBLE);
+        checkName.setVisibility(View.INVISIBLE);
+        checkPass.setVisibility(View.INVISIBLE);
+        checkPassC.setVisibility(View.INVISIBLE);
 
         backBtn.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -64,68 +71,44 @@ public class RegistActivity extends AppCompatActivity{
                 imm.hideSoftInputFromWindow(password.getWindowToken(),0);
                 imm.hideSoftInputFromWindow(passConf.getWindowToken(),0);
                 imm.hideSoftInputFromWindow(name.getWindowToken(),0);
+                checkInput();
                 return false;
             }
         });
-//        name.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if(!nameFlag){
-//                    name.setText("");
-//                    nameFlag = true;
-//                }
-//                return false;
-//            }
-//        });
-        name.addTextChangedListener(new TextWatcher() {
+        name.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
+            public void onFocusChange(View v, boolean hasFocus) {
+                checkInput();
             }
         });
-//        email.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if(!emailFlag){
-//                    email.setText("");
-//                    emailFlag = true;
-//                }
-//                return false;
-//            }
-//        });
-//
-//        password.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if(!passwordFlag){
-//                    password.setText("");
-//                    password.setInputType(0x00000081); // 숫자 비밀번호 방식(***)으로 변경
-//                    passwordFlag = true;
-//                }
-//                return false;
-//            }
-//        });
-//        passConf.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if(!passConfFlag){
-//                    passConf.setText("");
-//                    passConf.setInputType(0x00000081); // 숫자 비밀번호 방식(***)으로 변경
-//                    passConfFlag = true;
-//                }
-//                return false;
-//            }
-//        });
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                checkInput();
+            }
+        });
+        password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                checkInput();
+            }
+        });
+        passConf.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                checkInput();
+            }
+        });
+        passConf.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                // 뷰의 id를 식별, 키보드의 완료 키 입력 검출
+                if(v.getId()==R.id.joinPassConf && actionId== EditorInfo.IME_ACTION_DONE){
+                    checkInput();
+                }
+                return false;
+            }
+        });
         joinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -142,44 +125,56 @@ public class RegistActivity extends AppCompatActivity{
                     Toast.makeText(getApplicationContext(), "비밀번호를 입력하세요.",Toast.LENGTH_SHORT).show();
                 }else if( passConfContent.getBytes().length <= 0) {
                     Toast.makeText(getApplicationContext(), "비밀번호를 한번 더 입력하세요.", Toast.LENGTH_SHORT).show();
-                }else if(!checkEmail(emailContent)){
-                    Toast.makeText(getApplicationContext(), "아이디를 이메일 형식으로 입력해주세요.", Toast.LENGTH_SHORT).show();
-                }else if(!passConf.getText().toString().equals(password.getText().toString())){ // 비밀번호 확인 다를시
-                    Toast.makeText(getApplicationContext(),"비밀번호가 일치하지 않습니다",Toast.LENGTH_SHORT).show();
-                }else if(passContent.length()!=4){
-                    Toast.makeText(getApplicationContext(), "비밀번호는 4자리 입니다.", Toast.LENGTH_SHORT).show();
-                }else {
+                }else if(!checkInput()) {
+                    Toast.makeText(getApplicationContext(), "가입 정보를 정확하게 입력해 주세요", Toast.LENGTH_SHORT).show();
+                }else{
                     Toast.makeText(getApplicationContext(), "가입되었습니다", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(RegistActivity.this, LoginActivity.class));
                     overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
                     finish();
                 }
-//                else{
-//                switch (checkPass(passContent)){
-//                    case 0: // 통과
-//                        Toast.makeText(getApplicationContext(),"가입되었습니다",Toast.LENGTH_SHORT).show();
-//                        startActivity(new Intent(RegistActivity.this,LoginActivity.class));
-//                        overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
-//                        finish();
-//                        break;
-//                    case 1: // 숫자형식 ㄴㄴ
-//                        Toast.makeText(getApplicationContext(), "올바른 비밀번호 형식이 아닙니다.", Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case 2: // 4자리 ㄴㄴ
-//                        Toast.makeText(getApplicationContext(), "비밀번호는 4자리 입니다.", Toast.LENGTH_SHORT).show();
-//                        break;
-//                }
-//            }
             }
         });
 
     }
-
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         startActivity(new Intent(RegistActivity.this,LoginActivity.class));
         overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
         finish();
+    }
+    private boolean checkInput(){
+        String name = this.name.getText().toString();
+        String email = this.email.getText().toString();
+        String pass = this.password.getText().toString();
+        String passC = this.passConf.getText().toString();
+        int cnt=0;
+        if(!(Pattern.matches("^[a-zA-Z]*$",name)||Pattern.matches("^[가-힣]*$",name))){ // 한글 또는 영어가 아닐경우
+            checkName.setVisibility(View.VISIBLE);
+            cnt++;
+        }else{
+            checkName.setVisibility(View.INVISIBLE);
+        }
+        if(!(LoginActivity.checkEmail(email)||email.getBytes().length==0)){ // 이메일 형식이 아닐경우
+            checkEmail.setVisibility(View.VISIBLE);
+            cnt++;
+        }else{
+            checkEmail.setVisibility(View.INVISIBLE);
+        }
+        if(pass.length() != 4 && pass.length() != 0 ){ // 비밀번호가 4자리 이상 인 경우
+            checkPass.setVisibility(View.VISIBLE);
+            cnt++;
+        }else{
+            checkPass.setVisibility(View.INVISIBLE);
+        }
+        if(!passC.equals(password.getText().toString())){ // 비밀번호와 일치하지 않을 경우
+            checkPassC.setVisibility(View.VISIBLE);
+            cnt++;
+        }else{
+            checkPassC.setVisibility(View.INVISIBLE);
+        }
+        if(cnt==0)return true;
+        else return false;
     }
 }
