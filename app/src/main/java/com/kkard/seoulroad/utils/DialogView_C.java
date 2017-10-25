74e1721c5897f5e5210fd703e89632fbc39b0f94
whtnrms2017 <whtnrms2012@naver.com>
2017-10-25 17:48:28 +0900

    방문록 다이얼로그

--- app/src/main/java/com/kkard/seoulroad/utils/DialogView_C.java
+++ app/src/main/java/com/kkard/seoulroad/utils/DialogView_C.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.kkard.seoulroad.R;
@@ -23,12 +26,16 @@ public class DialogView_C extends Dialog {
     private TextView mTitleView;
     private TextView mContentView;
     private TextView mIdView;
-    private TextView mCountView;
+    private TextView mLikeCountView;
     private ImageView mImageView;
+    private ImageView mXBtn;
 
-    private Button mLeftButton;
+    private Button mLikeButton;
     private Button mRightButton;
     private Button mMiddleButton;
+
+    private RelativeLayout back;
+    private LinearLayout inside;
 
     private int mImage;
     private String mTitle;
@@ -40,7 +47,8 @@ public class DialogView_C extends Dialog {
     private View.OnClickListener mRightClickListener;
     private View.OnClickListener mMiddleClickListener;
 
-    private boolean flag = true;
+    private boolean dialogTypeFlag = true;
+    private boolean isClickLike = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +58,7 @@ public class DialogView_C extends Dialog {
         lpWindow.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         lpWindow.dimAmount = 0.8f;
         getWindow().setAttributes(lpWindow);
-        if(flag) { // 이미지 클릭 다이얼로그
+        if(dialogTypeFlag) { // 이미지 클릭 다이얼로그
             setContentView(R.layout.activity_c_dialogview);
             setLayout();
             setTitle(mTitle);
@@ -58,11 +66,28 @@ public class DialogView_C extends Dialog {
             setId(mId);
             setCount(mCount);
             setImage(mImage);
-            //setClickListener(mLeftClickListener , mRightClickListener);
+
+            mLikeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int mcnt;
+                    String cnt = mLikeCountView.getText().toString();
+                    cnt = cnt.substring(0,cnt.length()-1);
+                    mcnt = Integer.parseInt(cnt);
+                    if(isClickLike) { // 좋아요가 눌려있으면
+                        mLikeButton.setBackground(ContextCompat.getDrawable(view.getContext(), R.drawable.love_btn_black));
+                        isClickLike = false;
+                        mcnt--; mLikeCountView.setText(Integer.toString(mcnt)+"명");
+                    }else{
+                        mLikeButton.setBackground(ContextCompat.getDrawable(view.getContext(), R.drawable.love_btn));
+                        isClickLike = true;
+                        mcnt++; mLikeCountView.setText(Integer.toString(mcnt)+"명");}
+                }
+            });
         }
         else { // 버튼 3개 다이얼로그
             setContentView(R.layout.activity_regit_dialog);
-            setLayout(flag);
+            setLayout(dialogTypeFlag);
             setTitle(mTitle);
             setClickListener(mLeftClickListener ,mMiddleClickListener ,mRightClickListener);
         }
@@ -71,7 +96,7 @@ public class DialogView_C extends Dialog {
 // 이미지 클릭시 생성자
     public DialogView_C(Context context,int image, String title, String id,String count,String content) {
         // Dialog 배경을 투명 처리 해준다.
-        super(context , android.R.style.Theme_Translucent_NoTitleBar);
+        super(context,android.R.style.Theme_Translucent_NoTitleBar);
         mImage = image;
         mTitle = title;
         mId = id;
@@ -102,7 +127,7 @@ public class DialogView_C extends Dialog {
         this.mLeftClickListener = photoClick;
         this.mMiddleClickListener = galleryClick;
         this.mRightClickListener = cancelClick;
-        flag = false;
+        dialogTypeFlag = false;
     }
 
     @Override
@@ -116,9 +141,7 @@ public class DialogView_C extends Dialog {
         return super.dispatchTouchEvent(ev);
     }
 
-    private void setTitle(String title){
-        mTitleView.setText(title);
-    }
+    private void setTitle(String title){mTitleView.setText(title);}
     private void setContent(String content){
         mContentView.setText(content);
     }
@@ -128,16 +151,16 @@ public class DialogView_C extends Dialog {
 
     private void setClickListener(View.OnClickListener left , View.OnClickListener right){
         if(left!=null && right!=null){
-            mLeftButton.setOnClickListener(left);
+            mLikeButton.setOnClickListener(left);
             mRightButton.setOnClickListener(right);
         }else if(left!=null && right==null){
-            mLeftButton.setOnClickListener(left);
+            mLikeButton.setOnClickListener(left);
         }else {
 
         }
     }
     private void setClickListener(View.OnClickListener left , View.OnClickListener middle ,View.OnClickListener right){
-        mLeftButton.setOnClickListener(left);
+        mLikeButton.setOnClickListener(left);
         mMiddleButton.setOnClickListener(middle);
         mRightButton.setOnClickListener(right);
     }
@@ -147,17 +170,38 @@ public class DialogView_C extends Dialog {
      */
     // 이미지 클릭 초기화 함수
     private void setLayout(){
-        mTitleView = (TextView) findViewById(R.id.tv_title);
-        mContentView = (TextView) findViewById(R.id.tv_content);
-        mCountView = (TextView) findViewById(R.id.tv_heart_cnt);
-        mIdView = (TextView)findViewById(R.id.tv_id_dia);
+        mTitleView = (TextView) findViewById(R.id.dia_title);
+        mContentView = (TextView) findViewById(R.id.dia_content);
+        mLikeCountView = (TextView) findViewById(R.id.cnt_like);
+        mIdView = (TextView)findViewById(R.id.dia_id);
         mImageView = (ImageView)findViewById(R.id.image_dialog);
-        mLeftButton = (Button) findViewById(R.id.btn_heart);
+        mLikeButton = (Button) findViewById(R.id.btn_heart);
+        mXBtn = (ImageView)findViewById(R.id.dia_x_btn);
+        back = (RelativeLayout)findViewById(R.id.dialog_back);
+        inside = (LinearLayout)findViewById(R.id.dialog_inside);
+        inside.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 아무것도 안함
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+        mXBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
     }
     // 버튼 세개 초기화 함수
     private void setLayout(boolean flag){
         mTitleView = (TextView) findViewById(R.id.up_title);
-        mLeftButton = (Button) findViewById(R.id.takepicture);
+        mLikeButton = (Button) findViewById(R.id.takepicture);
         mMiddleButton = (Button) findViewById(R.id.fromgallery);
         mRightButton = (Button) findViewById(R.id.upcancel);
     }
